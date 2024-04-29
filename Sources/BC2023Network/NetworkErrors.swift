28d2651e1a12d9f2cc82cdacaa47908d2254c607
Guillermo Fdez <guillermofdez12@gmail.com>
2024-04-29 13:34:02 +0200

    Modificiacion de los NetworkError para las imagenes CDN

--- Sources/BC2023Network/NetworkErrors.swift
+++ Sources/BC2023Network/NetworkErrors.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-public enum NetworkError:Error {
+public enum NetworkError: Error {
     case general(Error)
     case status(Int)
     case json(Error)
@@ -15,23 +15,32 @@ public enum NetworkError:Error {
     case noHTTP
     case vapor(String, Int)
     case unknown
+    case imageConversionFailed
+    case encodingFailed
+    case serverError
     
-    public var description:String {
+    public var description: String {
         switch self {
         case .general(let error):
-            return "Error general \(error.localizedDescription)"
+            return "Error general: \(error.localizedDescription)"
         case .status(let int):
-            return "Error de Status: \(int)"
+            return "Error de estado: \(int)"
         case .json(let error):
-            return "Error en el JSON: \(error)"
+            return "Error en JSON: \(error)"
         case .dataNotValid:
-            return "Dato no válido"
+            return "Datos no válidos"
         case .noHTTP:
-            return "No es una conexión HTTP"
+            return "No es una respuesta HTTP"
         case .vapor(let reason, _):
-            return reason
+            return "Error de Vapor: \(reason)"
         case .unknown:
             return "Error desconocido"
+        case .imageConversionFailed:
+            return "La conversión de la imagen falló"
+        case .encodingFailed:
+            return "La codificación de los datos falló"
+        case .serverError:
+            return "Error del servidor"
         }
     }
 }
